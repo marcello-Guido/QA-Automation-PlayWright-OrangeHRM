b2d9aed49a7485ed16b68af6e18f00d1883008d9
unknown <mguido2024@gmail.com>
2025-11-19 12:04:53 -0300

    test fail

--- tests/bootcamp.spec.js
+++ tests/bootcamp.spec.js
@@ -1,15 +1,29 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('Access OrangeHRM', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-  await page.locator("[name='username']").fill('Admin');
-  await page.locator("[name='password']").fill('admin123');
-  await page.locator("[type='submit']").click();
+test.describe('Cenários de Login', () => {
+  test('Login com sucesso', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-  await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
-  await page.locator('h6.oxd-text.oxd-text--h6').waitFor();
-  
-});
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByPlaceholder("password").fill('admin123');
+    await page.locator("[type='submit']").click();
 
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+    await page.locator('h6.oxd-text.oxd-text--h6').waitFor();
+
+  });
+
+  test('Login com falha - Senha incorreta', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByPlaceholder("password").fill('admin');
+    await page.locator("[type='submit']").click();
+
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.locator('p.oxd-alert-content-text.oxd-text').waitFor()
+
+  });
+})
