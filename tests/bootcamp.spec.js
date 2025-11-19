ba19dccf33597a74eed303b0c4071081a0c9a2f6
unknown <mguido2024@gmail.com>
2025-11-19 10:59:07 -0300

    assert login

--- tests/bootcamp.spec.js
+++ tests/bootcamp.spec.js
@@ -6,9 +6,10 @@ test('Access OrangeHRM', async ({ page }) => {
 
   await page.locator("[name='username']").fill('Admin');
   await page.locator("[name='password']").fill('admin123');
-  await page.locator("[type='submit']").click()
+  await page.locator("[type='submit']").click();
 
-
+  await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+  await page.locator('h6.oxd-text.oxd-text--h6').waitFor();
   
 });
 
